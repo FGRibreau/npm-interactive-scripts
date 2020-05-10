59ca939bf9eb0585cb7bb85f9cdef2ee5b242fac
François-Guillaume Ribreau <github@fgribreau.com>
2020-05-10 13:12:13 +0200

    👋

---
+++ README.md
@@ -0,0 +1,20 @@
+# Interactive script runner for npm
+
+- Too many scripts inside your package.json?
+- Use `npm-scripts` or `ns` CLI for [fuzzy](https://github.com/mattyork/fuzzy) search
+
+## Installation
+
+```bash
+npm install --global npm-interactive-scripts
+```
+
+## Usage
+
+After installing globally with `npm` or `yarn`, run `ns` in your terminal.
+
+
+# Inspiration
+
+- [interactive-scripts](https://github.com/hawkins/interactive-scripts/)
+- [npm-scripts](https://github.com/knownasilya/npm-scripts)
---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+const path = require("path");
+const runAll = require("npm-run-all");
+
+const fuzzy = require('fuzzy');
+
+const inquirer = require("inquirer");
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+
+function promptUser(err, data) {
+  // Collect scripts
+  const scriptNames = Object.keys(data.scripts);
+
+  // Prompt user
+  inquirer
+    .prompt({
+      pageSize: 1000,
+      type: "autocomplete",
+      name: "script",
+      message: "Script to run",
+      source: (_, input) => Promise.resolve(fuzzy.filter(input || '', scriptNames).map(el => el.original))
+    })
+    .then(run)
+    .catch(console.error);
+}
+
+function run(val) {
+  runAll(val.script, {
+    stdout: process.stdout,
+    stderr: process.stderr
+  }).catch(() => {
+    // Do nothing
+  });
+}
+
+promptUser(null, require(path.join(process.cwd(), "package.json")));
